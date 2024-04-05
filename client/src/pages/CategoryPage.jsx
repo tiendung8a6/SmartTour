39323207a1249a68b1d5d2253178ef023622166f
tiendung8a6 <tiendung8a6@gmail.com>
2024-04-05 16:09:11 +0700

    fix client change query id to label

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,31 +1,34 @@
 import { Card, Pagination, PopularPosts, PopularWriters } from "../components";
 import { usePopularPost, usePosts } from "../hooks/post_hooks";
+import { CATEGORIES } from "../utils/dummyData";
 
 const CategoryPage = () => {
   const query = new URLSearchParams(window.location.search).get("cat");
+  const category = CATEGORIES.find((cat) => cat._id === query);
+  const label = category ? category.label : "";
+
   const { posts, numOfPages, setPage } = usePosts({
     writerId: "",
   });
   const popular = usePopularPost();
-
   const handlePageChange = (page) => {
     setPage(page);
   };
 
   return (
-    <div className='px-0 2xl:px-20'>
-      <div className='py-5'>
-        <h2 className='text-4xl 2xl:text-5xl font-semibold text-slate-800 dark:text-white'>
-          {query}
+    <div className="px-0 2xl:px-20">
+      <div className="py-5">
+        <h2 className="text-4xl 2xl:text-5xl font-semibold text-slate-800 dark:text-white">
+          {label}
         </h2>
       </div>
 
-      <div className='w-full flex flex-col md:flex-row gap-10 2xl:gap-20'>
+      <div className="w-full flex flex-col md:flex-row gap-10 2xl:gap-20">
         {/* LEFT */}
-        <div className='w-full md:w-2/3 flex flex-col gap-y-28 md:gap-y-14'>
+        <div className="w-full md:w-2/3 flex flex-col gap-y-28 md:gap-y-14">
           {posts?.length === 0 ? (
-            <div className='w-full h-full py-8 flex  justify-center'>
-              <span className='text-lg text-slate-500'>
+            <div className="w-full h-full py-8 flex  justify-center">
+              <span className="text-lg text-slate-500">
                 No Post Available for this category
               </span>
             </div>
@@ -35,7 +38,7 @@ const CategoryPage = () => {
                 <Card key={post?._id} post={post} />
               ))}
 
-              <div className='w-full flex items-cemter justify-center'>
+              <div className="w-full flex items-cemter justify-center">
                 <Pagination
                   totalPages={numOfPages}
                   onPageChange={handlePageChange}
@@ -46,7 +49,7 @@ const CategoryPage = () => {
         </div>
 
         {/* RIGHT */}
-        <div className='w-full md:w-1/4 flex flex-col gap-y-12'>
+        <div className="w-full md:w-1/4 flex flex-col gap-y-12">
           {/* POPULAR POSTS */}
           <PopularPosts posts={popular?.posts} />
 
