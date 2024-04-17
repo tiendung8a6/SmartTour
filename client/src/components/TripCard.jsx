d2a35b7ed3caeb5c022dc7262ccc0aa928017893
tiendung8a6 <tiendung8a6@gmail.com>
2024-04-17 23:58:20 +0700

    Style client Trips

--- client/src/components/TripCard.jsx
+++ client/src/components/TripCard.jsx
@@ -1,8 +1,21 @@
 import Markdown from "markdown-to-jsx";
 import React from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
+// import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
-
+import { Grid, Menu, Button, Text, rem } from "@mantine/core";
+// icon tabler
+import {
+  IconCirclesRelation,
+  IconPencil,
+  IconDots,
+  IconSettings,
+  IconChevronDown,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from "@tabler/icons-react";
 const TripCard = ({ trip, index }) => {
   const startDate = new Date(trip?.startDate);
   const endDate = new Date(trip?.endDate);
@@ -14,48 +27,166 @@ const TripCard = ({ trip, index }) => {
   return (
     <div
       key={trip?._id}
-      className={`w-full flex flex-col gap-8 items-center rounded 
-     md:flex-row
-        `}
-      //  ${index / 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"}
+      className={`w-full flex flex-col gap-8 items-center rounded
+      md:flex-row border `}
     >
-      <Link
-        to={`/trip/${trip._id}`}
-        className="w-full h-auto md:h-64 md:w-2/4 "
-      >
-        <img
-          src={trip?.image}
-          alt={trip?.tripName}
-          className="object-cover w-full h-full rounded"
-        />
-      </Link>
-
-      <div className="w-full md:w-2/4 flex flex-col gap-3">
-        <h6 className="text-xl 2xl:text-3xl font-semibold text-black dark:text-white text-justify">
+      <div className="w-full md:w-full flex flex-col gap-3 py-[5px] px-[20px]  ">
+        <h6 className=" text-[1.5rem] font-semibold text-[#0782c5] dark:text-white text-justify">
           {trip?.tripName.slice(0, 60) + "..."}
         </h6>
-        <div className="flex-1 overflow-hidden text-gray-600 dark:text-slate-500 text-sm text-justify">
-          <Markdown options={{ wrapper: "article" }}>
-            {trip?.city?.slice(0, 60) + "..."}
-          </Markdown>
-        </div>
+        <div className=" flex gap-2 flex-col mt[-10px]">
+          <span>{trip?.city?.slice(0, 60) + "..."}</span>
 
-        <div className="flex gap-2">
           <span className="text-sm text-gray-600">
             {new Date(trip?.startDate).toLocaleDateString("vi-VN")} -
             {new Date(trip?.endDate).toLocaleDateString("vi-VN")} (
             {diffDays === 0 ? "1 ngày" : `${diffDays} ngày`})
           </span>
+
+          <span className="flex">
+            <span className="bg-[#0782c5] border-[#0782c5] border rounded-full w-[24px] h-[24px] p-[2px]    ">
+              <IconCirclesRelation
+                stroke={2}
+                className="text-[white] mt-[1px] h-[17px] w-[17px] "
+              />
+            </span>
+            <span className="ml-[10px]">
+              <Link className="text-[#0782c5]">Chia sẻ </Link>
+            </span>
+          </span>
         </div>
 
-        <Link
+        {/* <div className="flex-1 overflow-hidden text-gray-600 dark:text-slate-500 text-[1rem] text-justify">
+          <Markdown options={{ wrapper: "article" }}>
+
+
+          </Markdown>
+        </div> */}
+
+        {/* <div className="flex gap-2">
+
+
+        </div> */}
+
+        {/* <Link
           to={`/trip/${trip._id}`}
           className="flex items-center gap-2 text-black dark:text-white"
         >
           <span className="underline">Chỉnh sửa chuyến đi</span>
           <AiOutlineArrowRight />
-        </Link>
+        </Link> */}
+        <Grid>
+          <Grid.Col span={{ base: 12, md: 6, lg: 4 }}>
+            <span className="flex">
+              <span className="bg-transparent border-[#0782c5] border rounded-full w-[24px] h-[24px] p-[2px]    ">
+                <IconPencil
+                  stroke={2}
+                  className="text-[#0782c5] mt-[1px] h-[17px] w-[17px] "
+                />
+              </span>
+              <span className="ml-[10px]">
+                <Link to={`/trip/${trip._id}`} className="text-[#0782c5]">
+                  Chỉnh sửa chuyến đi
+                </Link>
+              </span>
+            </span>
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, md: 6, lg: 4 }}>
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <Button className="flex bg-transparent hover:bg-transparent">
+                  <span className="bg-transparent border-[#0782c5] border rounded-full w-[24px] h-[24px] p-[2px]    ">
+                    <IconDots
+                      stroke={2}
+                      className="text-[#0782c5] mt-[1px] h-[17px] w-[17px] "
+                    />
+                  </span>
+                  <span className="ml-[10px]">
+                    <div className="text-[#0782c5] flex items-center">
+                      Chỉnh sửa chuyến đi{" "}
+                      <IconChevronDown
+                        stroke={2}
+                        className="text-[#0782c5] ml-1   mt-[1px] h-[17px] w-[17px] "
+                      />
+                    </div>
+                  </span>
+                </Button>
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <Menu.Label>Application</Menu.Label>
+                <Menu.Item
+                  leftSection={
+                    <IconSettings style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Settings
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconMessageCircle
+                      style={{ width: rem(14), height: rem(14) }}
+                    />
+                  }
+                >
+                  Messages
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconPhoto style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Gallery
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconSearch style={{ width: rem(14), height: rem(14) }} />
+                  }
+                  rightSection={
+                    <Text size="xs" c="dimmed">
+                      ⌘K
+                    </Text>
+                  }
+                >
+                  Search
+                </Menu.Item>
+
+                <Menu.Divider />
+
+                <Menu.Label>Danger zone</Menu.Label>
+                <Menu.Item
+                  leftSection={
+                    <IconArrowsLeftRight
+                      style={{ width: rem(14), height: rem(14) }}
+                    />
+                  }
+                >
+                  Transfer my data
+                </Menu.Item>
+                <Menu.Item
+                  color="red"
+                  leftSection={
+                    <IconTrash style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Delete my account
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Grid.Col>
+        </Grid>
       </div>
+
+      <Link
+        to={`/trip/${trip._id}`}
+        className="w-full h-auto md:h-64 md:w-1/4 lg:w-[380px] hidden md:block "
+      >
+        <img
+          src={trip?.image}
+          alt={trip?.tripName}
+          className="object-cover w-full md:h-[100%] rounded"
+        />
+      </Link>
     </div>
   );
 };
--- client/src/pages/NewTrip.jsx
+++ client/src/pages/NewTrip.jsx
@@ -3,7 +3,10 @@ import {
   Select,
   TextInput,
   useMantineColorScheme,
+  Grid,
+  Switch,
 } from "@mantine/core";
+import { IconCalendarEvent } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 import { BiImages } from "react-icons/bi";
@@ -68,101 +71,139 @@ const NewTrip = () => {
   };
 
   return (
-    <>
+    <div className="px-[100px] ">
       <p
         className={`${
           theme ? "text-white" : "text-slate-700"
         } text-lg pb-1 font-semibold `}
       >
-        Create a Trip
+        Thêm Chuyến Đi
       </p>
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
-        <TextInput
-          withAsterisk
-          label="tripName"
-          className="w-full flex-1"
-          placeholder="tripName"
-          defaultValue={tripName}
-          onChange={(e) => setTripName(e.target.value)}
-        />
-      </div>
+      <br />
 
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
-        <TextInput
-          withAsterisk
-          label="city"
-          className="w-full flex-1"
-          placeholder="city"
-          defaultValue={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-      </div>
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
-        <DateInput
-          clearable
-          withAsterisk
-          label="startDate"
-          className="w-full flex-1"
-          placeholder="startDate"
-          minDate={new Date()}
-          valueFormat="DD/MM/YYYY"
-          value={startDate}
-          onChange={(date) => setStartDate(date)}
-        />
-      </div>
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
-        <DateInput
-          clearable
-          withAsterisk
-          label="endDate"
-          className="w-full flex-1"
-          placeholder="endDate"
-          valueFormat="DD/MM/YYYY"
-          minDate={startDate}
-          value={endDate}
-          onChange={(date) => setEndDate(date)}
-        />
-      </div>
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-4">
-        <label
-          className="flex items-center gap-1 text-sm font-medium cursor-pointer"
-          htmlFor="imgUpload"
-        >
-          <input
-            type="file"
-            onChange={(e) => setFile(e.target.files[0])}
-            className="hidden"
-            id="imgUpload"
-            data-max-size="5120"
-            accept=".jpg, .png, .jpeg"
-          />
-          <BiImages />
-          <span>Image</span>
-          <span className="text-rose-500 mr-[10px]">*</span>
-          <div>
-            {fileURL && (
-              <img src={fileURL || file} alt="" className="w-20 h-20" />
-            )}
+      <Grid className="">
+        <Grid.Col span={{ base: 12, md: 7, lg: 7 }}>
+          <p>
+            Add a trip manually below or forward your confirmation emails to
+            plans@tripit.com, and we'll create the trip for you.
+          </p>
+
+          <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5 mt-6">
+            <TextInput
+              withAsterisk
+              label="Tên Chuyến Đi"
+              className="w-full flex-1"
+              placeholder="Tên Chuyến Đi"
+              defaultValue={tripName}
+              onChange={(e) => setTripName(e.target.value)}
+            />
           </div>
-        </label>
+
+          <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5 mt-6">
+            <TextInput
+              withAsterisk
+              label="Thành Phố"
+              className="w-full flex-1"
+              placeholder="Thành Phố"
+              defaultValue={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+          <Grid className="mt-6">
+            <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+              <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
+                <DateInput
+                  leftSection={
+                    <IconCalendarEvent className="text-[#107ac5]" size={24} />
+                  }
+                  clearable
+                  withAsterisk
+                  label="Ngày Bắt Đầu"
+                  className="w-full flex-1"
+                  placeholder="Ngày Bắt Đầu"
+                  minDate={new Date()}
+                  valueFormat="DD/MM/YYYY"
+                  value={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+              <div className="w-full flex flex-col md:flex-row flex-wrap gap-5 mb-5">
+                <DateInput
+                  leftSection={
+                    <IconCalendarEvent className="text-[#107ac5]" size={24} />
+                  }
+                  clearable
+                  withAsterisk
+                  label="Ngày Kết Thúc"
+                  className="w-full flex-1"
+                  placeholder="Ngày Kết Thúc"
+                  valueFormat="DD/MM/YYYY"
+                  minDate={startDate}
+                  value={endDate}
+                  onChange={(date) => setEndDate(date)}
+                />
+              </div>
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+
+        <Grid.Col span={{ base: 12, md: 3, lg: 3 }} offset={0.5}>
+          <div className="w-full  flex-col md:flex-row flex-wrap gap-5 mb-4">
+            <div>
+              {fileURL && (
+                <img src={fileURL || file} alt="" className="w-fit h-[250px]" />
+              )}
+            </div>
+            <label
+              className="flex  gap-1 text-sm font-medium cursor-pointer mt-6 "
+              htmlFor="imgUpload"
+            >
+              <input
+                type="file"
+                onChange={(e) => setFile(e.target.files[0])}
+                className="hidden"
+                id="imgUpload"
+                data-max-size="5120"
+                accept=".jpg, .png, .jpeg"
+              />
+              <BiImages />
+              <span>Image</span>
+              <span className="text-rose-500 mr-[10px]">*</span>
+            </label>
+          </div>
+        </Grid.Col>
+      </Grid>
+
+      <div className="w-full flex items-end justify-start mt-6">
+        <Switch color="indigo" label="Công Khai chuyển đi" />
+      </div>
+      <div className="flex justify-start gap-3">
+        <div className=" flex items-end justify-start mt-6">
+          <Button
+            variant="light"
+            color="indigo"
+            size="md"
+            radius="md"
+            onClick={() => handleSubmit()}
+          >
+            Lưu
+          </Button>
+        </div>
+
+        <div className=" flex items-end justify-start mt-6">
+          <Link to="/trip">
+            <Button variant="outline" color="Red" size="md" radius="md">
+              Hủy
+            </Button>
+          </Link>
+        </div>
       </div>
 
-      <div className="w-full flex items-end justify-end mt-6">
-        <Button
-          className={theme ? "bg-blue-600" : "bg-black"}
-          onClick={() => handleSubmit()}
-        >
-          Submit Trip
-        </Button>
-      </div>
-      <div className="w-full flex items-end justify-end mt-6">
-        <Link to="/trip">
-          <Button className={theme ? "bg-blue-600" : "bg-black"}>Cancel</Button>
-        </Link>
-      </div>
       <LoadingClient visible={isPending} />
       <Toaster richColors />
-    </>
+    </div>
   );
 };
 
