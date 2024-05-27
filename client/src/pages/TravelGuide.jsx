f8d6cfa74e4ad3c059ad8465d79cfdabd28cec89
tiendung8a6 <tiendung8a6@gmail.com>
2024-05-27 17:24:56 +0700

    style AI Travel

--- client/src/pages/TravelGuide.jsx
+++ client/src/pages/TravelGuide.jsx
@@ -36,7 +36,7 @@ const Header = ({ place, setPlace, handleSearch }) => (
         className="flex-grow min-w-0 px-4 py-2 mr-4 text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <button
-        className="px-4 py-2 text-lg text-white bg-orange-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="px-4 py-2 text-lg text-white bg-sky-500 hover:bg-sky-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500/50"
         onClick={() => handleSearch()}
       >
         Khám phá
