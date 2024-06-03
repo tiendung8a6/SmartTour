92421d7c523031d5e11056d86e2ede13f60e9df1
tiendung8a6 <tiendung8a6@gmail.com>
2024-06-04 04:40:10 +0700

    style admin analytics

--- admin/src/components/Chart.jsx
+++ admin/src/components/Chart.jsx
@@ -13,24 +13,32 @@ import {
 const Chart = ({ dt }) => {
   return (
     <ResponsiveContainer width={"100%"} height={500} className="mt-5">
-      <LineChart
-        width={500}
-        height={300}
-        data={dt}
-        margin={{
-          top: 5,
-          right: 5,
-          left: 50,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <YAxis />
-        <XAxis dataKey="_id" />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="Total" stroke="#0088FE" />
-      </LineChart>
+      {dt?.length > 0 ? (
+        <LineChart
+          width={500}
+          height={300}
+          data={dt}
+          margin={{
+            top: 5,
+            right: 5,
+            left: 50,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <YAxis />
+          <XAxis dataKey="_id" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Total" stroke="#0088FE" />
+        </LineChart>
+      ) : (
+        <img
+          src="https://user-images.githubusercontent.com/15953522/49493502-63e21d00-f882-11e8-911c-1d7655f393e8.png"
+          alt="No Data"
+          className="w-full h-full opacity-50"
+        />
+      )}
     </ResponsiveContainer>
   );
 };
