ab9a08243775321836a87f33b15000ebce13aff3
tiendung8a6 <tiendung8a6@gmail.com>
2024-06-07 17:59:09 +0700

    style dialog

--- admin/src/components/ConfirmDialog.jsx
+++ admin/src/components/ConfirmDialog.jsx
@@ -7,16 +7,16 @@ const ConfirmDialog = ({ message, opened, close, handleClick }) => {
 
       <div className="w-full flex gap-4 justify-end mt-8">
         <Button
-          className="text-sky-700	 bg-sky-500 hover:bg-sky-900 hover:text-sky-100 bg-opacity-25 text-sm font-medium"
-          onClick={handleClick}
-        >
-          Xác Nhận
-        </Button>
-        <Button
           className="border border-slate-300 text-slate-600 hover:bg-gray-100 hover:text-black text-sm"
           onClick={close}
         >
           Hủy
+        </Button>
+        <Button
+          className="text-white bg-black hover:bg-gray-800 hover:text-sky-100 text-sm font-medium"
+          onClick={handleClick}
+        >
+          Xác Nhận
         </Button>
       </div>
     </Modal>
--- client/src/components/ConfirmDialog.jsx
+++ client/src/components/ConfirmDialog.jsx
@@ -4,19 +4,18 @@ const ConfirmDialog = ({ message, opened, close, handleClick }) => {
   return (
     <Modal opened={opened} onClose={close} title="Xác Nhận Hành Động" centered>
       <p className="text-base">{message}</p>
-
       <div className="w-full flex gap-4 justify-end mt-8">
         <Button
-          className="bg-red-500 text-white hover:bg-red-700 text-sm font-medium"
-          onClick={handleClick}
-        >
-          Xác Nhận
-        </Button>
-        <Button
-          className=" bg-sky-600 text-white hover:bg-sky-800 border border-slate-300 text-sm"
+          className=" bg-white text-slate-600 hover:bg-gray-100 hover:text-black border border-slate-300 text-sm"
           onClick={close}
         >
           Hủy
+        </Button>
+        <Button
+          className="bg-sky-600 text-white hover:bg-sky-500 text-sm font-medium"
+          onClick={handleClick}
+        >
+          Xác Nhận
         </Button>
       </div>
     </Modal>
