2138a3de6137ceca53107662f9d79915f3f27d92
tiendung8a6 <tiendung8a6@gmail.com>
2024-06-20 13:15:02 +0700

    fix ver2 trip hook

--- client/src/hooks/trip_hooks.js
+++ client/src/hooks/trip_hooks.js
@@ -54,7 +54,7 @@ export const useTrips = () => {
   useEffect(() => {
     if (errorOccurred) {
       signOut();
-      // navigate("/sign-in");
+      // navigate("/sign-in"); //NEW --Ver2 (fix)
       // toast.error("Something went wrong.");
     }
   }, [errorOccurred, signOut]);
