45734b60919780c811c737f3c4717d2422be1a0c
tiendung8a6 <tiendung8a6@gmail.com>
2024-06-23 10:36:54 +0700

    style api send otp email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -32,23 +32,80 @@ export const sendVerificationEmail = async (user, res, token) => {
   const mailOptions = {
     from: AUTH_EMAIL,
     to: email,
-    subject: "Email Verification",
-    html: `<div
-    style='font-family: Arial, sans-serif; font-size: 20px; color: #333; background-color: #f7f7f7; padding: 20px; border-radius: 5px;'>
-    <h3 style="color: rgb(8, 56, 188)">Please verify your email address</h3>
-    <hr>
-    <h4>Hi ${name},</h4>
-    <p>
-        Please verify your email address with the OTP.
-        <br>
-        <h1 styles='font-size: 20px; color: rgb(8, 56, 188);'>${otp}</h1>
-    <p>This OTP <b>expires in 2 mins</b></p>
-    </p>
-    <div style="margin-top: 20px;">
-        <h5>Regards,</h5>
-        <h5>SmartTour</h5>
-    </div>
-</div>`,
+    subject: "Xác thực tài khoản SmartTour của bạn",
+    html: `<body style="background-color:#f6f9fc;padding:10px 0">
+    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation"
+        style="max-width:37.5em;background-color:#ffffff;border:1px solid #f0f0f0;padding:45px">
+        <tbody>
+            <tr style="width:100%">
+                <td>
+                    <div
+                        style="font-family:HelveticaNeue-Medium,Helvetica,Arial,sans-serif; width: 212px; height: 88px; display: block; margin: 0 auto; text-align: center; line-height: 88px;">
+                        <span style="font-size: 30px; font-weight: bold;">Smart</span>
+                        <span style="font-size: 36px; font-weight: bold ; color:#1778d8e5;">Tour</span>
+                    </div>
+                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Xin chào ${name},</p>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Mã OTP xác thực tài khoản của bạn là:</p>
+
+                                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0"
+                                        role="presentation"
+                                        style="background:rgba(0,0,0,.05);border-radius:4px;margin:16px auto 14px;vertical-align:middle;width:280px">
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <p
+                                                        style="font-size:32px;line-height:40px;margin:0 auto;color:#000;display:inline-block;font-family:HelveticaNeue-Bold;font-weight:700;letter-spacing:6px;padding-bottom:8px;padding-top:8px;width:100%;text-align:center">
+                                                        ${otp}</p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Để xác thưc tài khoản bạn trên SmartTour, chúng tôi cần xác minh địa chỉ
+                                        email của bạn. Hãy dán mã này vào trình duyệt.</p>
+
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Đây là mã dùng một lần có hiệu lực trong 2 phút.
+                                    </p>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Để giữ an toàn cho tài khoản của bạn, vui lòng không chia sẻ mã hoặc chuyển
+                                        tiếp email này cho bất kỳ ai.
+                                    </p>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Liên hệ <a href="tiendung8a6@gmail.com"
+                                            style="color:#444;text-decoration:underline"
+                                            target="_blank">tiendung8a6@gmail.com</a> nếu gặp khó khăn trong quá trình
+                                        xác thực.</p>
+                                    <p
+                                        style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        Cảm ơn bạn!</p>
+                                    <p
+                                        style="font-size:16px;line-height:2px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">
+                                        SmartTour</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p
+        style="font-size:12px;line-height:23px;margin:0;color:#000;font-weight:800;letter-spacing:0;margin-top:20px;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;text-align:center;text-transform:uppercase">
+        © Copyright 2024 SmartTour. All rights reserved.</p>
+</body>`,
   };
 
   try {
